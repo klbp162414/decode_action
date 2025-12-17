10ff958792725170b08930e6eed8d257bf467e81
action <action@github.com>
2025-12-17 10:19:38 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,4 +1,4 @@
-//Sun Dec 14 2025 11:33:19 GMT+0000 (Coordinated Universal Time)
+//Wed Dec 17 2025 10:19:38 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
 const $ = new Env("掌心临海");
